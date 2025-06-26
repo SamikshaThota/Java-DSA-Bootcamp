fa18c45ad4d3c310551da569a804eb37c4904d46
SamikshaThota <sami.yosi2003@gmail.com>
2025-06-27 05:03:03 +0530

    Create primitive-datatypes.java

---
+++ primitive-datatypes.java
@@ -0,0 +1,16 @@
+public class PrimitiveDatatypes{
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        float f=24.32f;
+        System.out.println(f);
+        char c='A';
+        System.out.println(c);
+        double d=34.735643;
+        System.out.println(d);
+        long l=341279463L;
+        System.out.println(l);
+        boolean b=true;
+        System.out.println(b);
+    }
+}
